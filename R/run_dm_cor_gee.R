0e171b57858637ae17a7bba582f10db944b1109b
Emily <emilymmpalmer@gmail.com>
2021-12-01 14:38:10 -0800

    README and use here:: to source code

--- R/run_dm_cor_gee.R
+++ R/run_dm_cor_gee.R
@@ -1,6 +1,6 @@
 
 # Can load practice data from https://github.com/lichen-lab/glmmTree in file glmmTree_data.R
-source('~/Desktop/ResearchFall2021/R/glmmTree_data.R')
+source(here::here("R","glmmTree_data.R"))
 # Filtered to have 50 samples and 48 OTUs so computation goes quickly 
 
 
