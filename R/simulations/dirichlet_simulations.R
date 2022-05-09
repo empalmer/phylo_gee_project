9e778ca69b031c48b76ad879463adae4fec4ad5e
Emily Palmer <emilymmpalmer@gmail.com>
2022-05-09 15:18:45 -0700

    Issue of dirichlet simulation found
    Alpha was generated incorrectly on the simulation (wrong dimension of matrix, wrong sample alpha values) Make sure it is nrow = p, ncol = n, byrow = T

--- R/simulations/dirichlet_simulations.R
+++ R/simulations/dirichlet_simulations.R
@@ -1,18 +1,32 @@
 # A package for randomly sampling dirichlet random variables. 
 # install.packages('DirichletReg')
+# needed for rdirichlet() fxn
 library(DirichletReg)
 
 
-n <- 1000
+# Set parameters (n, p, q) ------------------------------------------------
+
+n <- 100
 p <- 15
 q <- 1
 
-# include intercept? no, should be default to make in model matrix. 
+
 # X is for each sample
+# single Binary covariate
+# Almost even split of case/control
 X <- c(rep(0, .4*n), rep(1, .6*n))
-# beta is for each taxa
+# Set betas (need one for each p)
 beta <- c(rep(-1, 5), rep(2, 10))
 
+
+# Calculate alpha ---------------------------------------------------------
+source(here::here("R", "helpers.R"))
+source(here::here("R", "dirichlet_functions.R"))
+
+# True eta has no intercept included 
+eta <- get_eta(X, beta, n, p)
+# log(alpha) = Xbeta
+alpha <- exp(eta)
 
 
 # Generate Dirichlet RV ---------------------------------------------------
@@ -22,7 +36,8 @@ beta <- c(rep(-1, 5), rep(2, 10))
 # Then combine alphas to one vector. 
 
 y_i <- list()
-mat <- matrix(alpha, nrow = n, ncol = p, byrow = T)
+mat <- matrix(alpha, nrow = p, ncol = n, byrow = T)
+mat[1,]
 for (i in 1:1000) {
   alphai <- mat[i, ]
   y_i[[i]] <- rdirichlet(1, alphai)
