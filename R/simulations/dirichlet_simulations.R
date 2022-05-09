a11e349ed721e693e01920f62e2462308cbc4eb6
Emily Palmer <emilymmpalmer@gmail.com>
2022-05-09 14:52:24 -0700

    Simulations: Dirichlet and MVN simulations into different files

--- R/simulations/dirichlet_simulations.R
+++ R/simulations/dirichlet_simulations.R
@@ -1,1 +1,31 @@
- 
+# A package for randomly sampling dirichlet random variables. 
+# install.packages('DirichletReg')
+library(DirichletReg)
+
+
+n <- 1000
+p <- 15
+q <- 1
+
+# include intercept? no, should be default to make in model matrix. 
+# X is for each sample
+X <- c(rep(0, .4*n), rep(1, .6*n))
+# beta is for each taxa
+beta <- c(rep(-1, 5), rep(2, 10))
+
+
+
+# Generate Dirichlet RV ---------------------------------------------------
+
+# Simulate from Dirichlet distribution given alpha, n, p
+# Need to draw n different samples, since each sample is dirichlet distributed
+# Then combine alphas to one vector. 
+
+y_i <- list()
+mat <- matrix(alpha, nrow = n, ncol = p, byrow = T)
+for (i in 1:1000) {
+  alphai <- mat[i, ]
+  y_i[[i]] <- rdirichlet(1, alphai)
+}
+
+ys <- unlist(y_i)
--- R/simulations/simulation.R
+++ R/simulations/simulation.R
@@ -1,18 +1,13 @@
-# A package for randomly sampling dirichlet random variables. 
-# install.packages('DirichletReg')
-library(DirichletReg)
 
-
-# Set up simulation -------------------------------------------------------
-
-# Get a distance matrix from phyloseq package 
+# Distance matrix ---------------------------------------------------------
+# Get a distance matrix from phyloseq package
 data("GlobalPatterns")
-#devtools::install_github("david-barnett/microViz@0.9.1")
+# devtools::install_github("david-barnett/microViz@0.9.1")
 library(microViz)
 GlobalPatterns
-GP = ps_filter(GlobalPatterns, SampleType == "Feces")
-gp1  = transform_sample_counts(GP, function(x) x / sum(x) )
-filtered <-  filter_taxa(gp1, function(x) mean(x) > 1e-4, TRUE)
+GP <- ps_filter(GlobalPatterns, SampleType == "Feces")
+gp1 <- transform_sample_counts(GP, function(x) x / sum(x))
+filtered <- filter_taxa(gp1, function(x) mean(x) > 1e-4, TRUE)
 filtered
 
 library(adephylo)
@@ -21,10 +16,12 @@ dim(D)
 set.seed(506)
 i <- sample(1:329, 15)
 
-D_sim <- D[i,i]
+D_sim <- D[i, i]
 dim(D_sim)
 
-# set parameters
+
+
+# Set omega, rho ----------------------------------------------------------
 omega <- 1
 rho <- 30
 
@@ -32,84 +29,83 @@ n <- 1000
 p <- 15
 q <- 1
 
-# include intercept? 
+# include intercept? no, should be default to make in model matrix. 
 # X is for each sample
-X <- c(rep(0, 400), rep(1,600))
-# beta is for each taxa 
-beta <- c(rep(-1, 5), rep(2,10))
+X <- c(rep(0, 400), rep(1, 600))
+# beta is for each taxa
+beta <- c(rep(-1, 5), rep(2, 10))
 
 
-# source helper functions 
-source(here::here("R","dm_cor_gee_clean.R"))
 
-eta <- get_eta(X, beta , n, p)
+# Set up correlation matrix -----------------------------------------------
+# Source dirichlet equation helpers 
+source(here::here("R", "helpers.R"))
+source(here::here("R", "dirichlet_functions.R"))
+
+eta <- get_eta(X, beta, n, p)
 alpha <- exp(eta)
 alpha
 summary(alpha)
 alpha0 <- rowSums(matrix(alpha, nrow = p, byrow = T))
 
-y_mean <- alpha/alpha0  
+y_mean <- alpha / alpha0
 
 
+
+# Correlation matrix, based on alpha, n, p, rho, omega
 library(Matrix)
 # dirichlet correlation (block diagonal of different blocks for each sample)
-cor_dirichlet_list <- get_dirichlet_cor(alpha,n,p)
-Rs <- purrr::map(cor_dirichlet_list, ~ .x*omega + (1-omega)*exp(-2*rho*D_sim))
+cor_dirichlet_list <- get_dirichlet_cor(alpha, n, p)
+Rs <- purrr::map(cor_dirichlet_list, ~ .x * omega + (1 - omega) * exp(-2 * rho * D_sim))
 R <- bdiag(Rs)
 
+
+# Covariance matrix. 
+# Need to calculate variance too since multivatite normal needs covariance not correlation
 # 1/sqrt(Var_D)
-A <- Diagonal(n*p)
-diag(A) <- sqrt(var_dirichlet(alpha,n,p))
+A <- Diagonal(n * p)
+diag(A) <- sqrt(var_dirichlet(alpha, n, p))
 # sqrt(Var_D) * R * (sqrt(Var_D))
 V <- A %*% R %*% A
 
 
-# Generate 1 sample with the mean and variance structure 
+# Simulate from multivariate normal ---------------------------------------
+# Generate 1 sample with the mean and variance structure
 library(MASS)
-ys <- mvrnorm(1, y_mean, V )
+ys <- mvrnorm(1, y_mean, V)
 ys
 
 
-# Try dirichlet now. 
-y_i <- list()
-mat <- matrix(alpha, nrow = n, ncol = p, byrow = T)
-for(i in 1:1000){
-  alphai <- mat[i,]
-  y_i[[i]] <- rdirichlet(1, alphai)
-}
+# Standardize sampled values ----------------------------------------------
 
-ys <- unlist(y_i)
-
-# Standardizing: 
-# How much standardizing is needed? 
-
-
-
-# Set any negative ys to 0. 
+# How much standardizing is needed?
 mean(ys < 0)
+# Set any negative ys to 0.
 ys_pos <- ifelse(ys < 0, 0, ys)
 
 
-# Do col sums need to add to 1? 
-# What do colsums look like? 
-rowSums(matrix(ys, nrow = n, ncol =p, byrow = T))
+# Do col sums need to add to 1?
+# What do colsums look like?
+rowSums(matrix(ys, nrow = n, ncol = p, byrow = T))
 
 
 
-# Shouldnt these be close to 1?  
-# Standardize: 
-ys_one <- data.frame(id = rep(1:100, each = 15), 
-                     ys_pos) %>%
+# Shouldnt these be close to 1?
+# Standardize:
+ys_one <- data.frame(
+  id = rep(1:100, each = 15),
+  ys_pos
+) %>%
   group_by(id) %>%
-  mutate(ysum = sum(ys_pos)) %>% 
-  mutate(ys_one = ys_pos/ ysum) %>% 
+  mutate(ysum = sum(ys_pos)) %>%
+  mutate(ys_one = ys_pos / ysum) %>%
   pull(ys_one)
 ys_one
 
 rowSums(matrix(ys_one, nrow = n, ncol = p, byrow = T))
 
 # Check if y has the same moments now? how to do this
-# since cant get alpha from y? right? 
+# since cant get alpha from y? right?
 
 
 
@@ -118,16 +114,17 @@ rowSums(matrix(ys_one, nrow = n, ncol = p, byrow = T))
 
 # Run model ---------------------------------------------------------------
 
-
-# Try simulation! 
-source(here::here("R","dm_cor_gee_clean.R"))
-model_output <- dm_cor_gee(Y = ys, X = X,
-                           sample_id = rep(1:n, each = 15), 
-                           ASV_id = rep(letters[1:15], n),
-                           distance_matrix = D_sim, 
-                           max_iter = 20, tol = .00001, 
-                           gamma = 1, lambda = .0001, 
-                           save_beta = T, intercept = T)
+# Try simulation!
+source(here::here("R", "dm_cor_gee_clean.R"))
+model_output <- dm_cor_gee(
+  Y = ys, X = X,
+  sample_id = rep(1:n, each = 15),
+  ASV_id = rep(letters[1:15], n),
+  distance_matrix = D_sim,
+  max_iter = 20, tol = .00001,
+  gamma = 1, lambda = .0001,
+  save_beta = T, intercept = T
+)
 
 model_output$betas
 descr <- "Simulation, only dirichlet"
